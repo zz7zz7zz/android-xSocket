44a8dbed3e9f5b42e347b664b92ca836e99d8c59
Long <longhui.yang@inveno.cn>
2017-12-05 20:25:53 +0800

    init project !

---
+++ net_socket_client/src/main/java/com/open/net/client/impl/udp/nio/UdpNioClient.java
@@ -0,0 +1,194 @@
+package com.open.net.client.impl.udp.nio;
+
+import com.open.net.client.structures.BaseClient;
+import com.open.net.client.structures.BaseMessageProcessor;
+import com.open.net.client.structures.IConnectListener;
+import com.open.net.client.structures.TcpAddress;
+import com.open.net.client.structures.message.Message;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+
+/**
+ * author       :   long
+ * created on   :   2017/11/30
+ * description  :   NioClient
+ */
+
+public final class UdpNioClient extends BaseClient {
+
+    private final String TAG="NioClient";
+
+    //-------------------------------------------------------------------------------------------
+    private UdpNioConnector mConnector;
+
+    public UdpNioClient(BaseMessageProcessor mMessageProcessor, IConnectListener mConnectListener) {
+        super(mMessageProcessor);
+        mConnector = new UdpNioConnector(this,mConnectListener);
+    }
+
+    //-------------------------------------------------------------------------------------------
+    private DatagramChannel mSocketChannel;
+    private ByteBuffer mReadByteBuffer  = ByteBuffer.allocate(64*1024);
+    private ByteBuffer mWriteByteBuffer = ByteBuffer.allocate(64*1024);
+
+    public void init(DatagramChannel socketChannel) {
+        this.mSocketChannel = socketChannel;
+    }
+
+    @Override
+    public void onCheckConnect() {
+        mConnector.checkConnect();
+    }
+
+    @Override
+    public void onClose() {
+        mSocketChannel = null;
+    }
+
+    public boolean onRead() {
+        boolean readRet = true;
+        try{
+            mReadByteBuffer.clear();
+            int readTotalLength = 0;
+            int readReceiveLength = 0;
+            while (true){
+                int readLength = mSocketChannel.read(mReadByteBuffer);//客户端关闭连接后，此处将抛出异常/或者返回-1
+                if(readLength == -1){
+                    readRet = false;
+                    break;
+                }
+                readReceiveLength += readLength;
+                //如果一次性读满了，则先回调一次，然后接着读剩下的，目的是为了一次性读完单个通道的数据
+                if(readReceiveLength == mReadByteBuffer.capacity()){
+                    mReadByteBuffer.flip();
+                    if(mReadByteBuffer.remaining() > 0){
+                        this.mMessageProcessor.onReceiveData(this, mReadByteBuffer.array(), 0 , mReadByteBuffer.remaining());
+                    }
+                    mReadByteBuffer.clear();
+                    readReceiveLength = 0;
+                }
+
+                if(readLength > 0){
+                    readTotalLength += readLength;
+                }else {
+                    break;
+                }
+            }
+
+            mReadByteBuffer.flip();
+            if(mReadByteBuffer.remaining() > 0){
+                this.mMessageProcessor.onReceiveData(this, mReadByteBuffer.array(), 0 , mReadByteBuffer.remaining());
+            }
+            mReadByteBuffer.clear();
+
+        }catch (Exception e){
+            e.printStackTrace();
+            readRet = false;
+        }
+
+        mMessageProcessor.onReceiveMessages(this);
+        //退出客户端的时候需要把要写给该客户端的数据清空
+        if(!readRet){
+            Message msg = pollWriteMessage();
+            while (null != msg) {
+                removeWriteMessage(msg);
+                msg= pollWriteMessage();
+            }
+        }
+        return readRet;
+    }
+
+    public boolean onWrite() {
+        boolean writeRet = true;
+        Message msg = pollWriteMessage();
+        try {
+            while (null != msg){
+                //如果消息块的大小超过缓存的最大值，则需要分段写入后才丢弃消息，不能在数据未完全写完的情况下将消息丢弃;avoid BufferOverflowException
+                if(mWriteByteBuffer.capacity() < msg.length){
+
+                    int offset = 0;
+                    int leftLength = msg.length;
+                    int writtenTotalLength;
+
+                    while(true){
+
+                        int putLength = leftLength > mWriteByteBuffer.capacity() ? mWriteByteBuffer.capacity() : leftLength;
+                        mWriteByteBuffer.put(msg.data,offset,putLength);
+                        mWriteByteBuffer.flip();
+                        offset      += putLength;
+                        leftLength  -= putLength;
+
+                        int writtenLength   = mSocketChannel.write(mWriteByteBuffer);//客户端关闭连接后，此处将抛出异常
+                        writtenTotalLength  = writtenLength;
+
+                        while(writtenLength > 0 && mWriteByteBuffer.hasRemaining()){
+                            writtenLength       = mSocketChannel.write(mWriteByteBuffer);
+                            writtenTotalLength += writtenLength;
+                        }
+                        mWriteByteBuffer.clear();
+
+                        if(leftLength <=0){
+                            break;
+                        }
+                    }
+                }else{
+                    mWriteByteBuffer.put(msg.data,msg.offset,msg.length);
+                    mWriteByteBuffer.flip();
+
+                    int writtenLength      = mSocketChannel.write(mWriteByteBuffer);//客户端关闭连接后，此处将抛出异常
+                    int writtenTotalLength = writtenLength;
+
+                    while(writtenLength > 0 && mWriteByteBuffer.hasRemaining()){
+                        writtenLength       = mSocketChannel.write(mWriteByteBuffer);
+                        writtenTotalLength += writtenLength;
+                    }
+                    mWriteByteBuffer.clear();
+                }
+
+                removeWriteMessage(msg);
+                msg = pollWriteMessage();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            writeRet = false;
+        }
+
+        //退出客户端的时候需要把要写给该客户端的数据清空
+        if(!writeRet){
+            if(null != msg){
+                removeWriteMessage(msg);
+            }
+            msg= pollWriteMessage();
+            while (null != msg) {
+                removeWriteMessage(msg);
+                msg= pollWriteMessage();
+            }
+        }
+
+        return writeRet;
+    }
+
+    //-------------------------------------------------------------------------------------------
+    public void setConnectAddress(TcpAddress[] tcpArray ){
+        mConnector.setConnectAddress(tcpArray);
+    }
+
+    public void setConnectTimeout(long connect_timeout ){
+        mConnector.setConnectTimeout(connect_timeout);
+    }
+
+    public void connect(){
+        mConnector.connect();
+    }
+
+    public void disconnect(){
+        mConnector.disconnect();
+    }
+
+    public void reconnect(){
+        mConnector.reconnect();
+    }
+}
---
+++ net_socket_client/src/main/java/com/open/net/client/impl/udp/nio/processor/SocketProcessor.java
@@ -0,0 +1,176 @@
+package com.open.net.client.impl.udp.nio.processor;
+
+import com.open.net.client.impl.udp.nio.IUdpNioConnectListener;
+import com.open.net.client.impl.udp.nio.UdpNioClient;
+import com.open.net.client.structures.BaseClient;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.DatagramChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.util.Iterator;
+
+/**
+ * author       :   long
+ * created on   :   2017/11/30
+ * description  :   连/读/写 处理器
+ */
+
+public final class SocketProcessor {
+
+    private static int G_SOCKET_ID = 0;
+
+    private int mSocketId;
+    private String  mIp ="192.168.1.1";
+    private int     mPort =9999;
+    private long   connect_timeout = 10000;
+
+    private BaseClient mClient;
+    private IUdpNioConnectListener mNioConnectListener;
+
+    private DatagramChannel mSocketChannel;
+    private Selector   mSelector;
+
+    private ConnectRunnable mConnectProcessor;
+    private Thread mConnectThread =null;
+
+    private boolean closed = false;
+
+    public SocketProcessor(String mIp, int mPort, long   connect_timeout , BaseClient mClient, IUdpNioConnectListener mNioConnectListener) {
+        this.mIp = mIp;
+        this.mPort = mPort;
+        this.connect_timeout = connect_timeout;
+        this.mClient = mClient;
+        this.mNioConnectListener = mNioConnectListener;
+        G_SOCKET_ID++;
+        mSocketId = G_SOCKET_ID;
+    }
+
+    public void start(){
+        mConnectProcessor = new ConnectRunnable();
+        mConnectThread = new Thread(mConnectProcessor);
+        mConnectThread.start();
+    }
+
+    public synchronized void close(){
+        closed = true;
+
+        if(null!= mSocketChannel) {
+            try {
+                SelectionKey key = mSocketChannel.keyFor(mSelector);
+                if(null != key){
+                    key.cancel();
+                }
+                mSelector.close();
+                mSocketChannel.socket().close();
+                mSocketChannel.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+        mSocketChannel = null;
+        mSelector = null;
+
+        wakeUp();
+    }
+
+    public void wakeUp(){
+        if(null !=mConnectProcessor){
+            mConnectProcessor.wakeUp();
+        }
+    }
+
+    public void onSocketExit(int exit_code){
+        close();
+        System.out.println("client mSocketId " + mSocketId);
+        if(null != mNioConnectListener){
+            mNioConnectListener.onConnectFailed(SocketProcessor.this);
+        }
+    }
+
+    private class ConnectRunnable implements Runnable {
+
+        public void wakeUp(){
+            if(null != mSelector){
+                mSelector.wakeup();
+            }
+        }
+
+        @Override
+        public void run() {
+            try {
+
+                mSelector = Selector.open();
+                mSocketChannel = DatagramChannel.open();
+                mSocketChannel.configureBlocking(false);
+
+                InetSocketAddress address=new InetSocketAddress(mIp, mPort);
+//                mSocketChannel.socket().bind(address);
+                mSocketChannel.connect(address);//可以将DatagramChannel“连接”到网络中的特定地址的。由于UDP是无连接的，连接到特定地址并不会像TCP通道那样创建一个真正的连接。而是锁住DatagramChannel ，让其只能从特定地址收发数据。
+                mSocketChannel.register(mSelector, SelectionKey.OP_READ,mClient);
+
+                ((UdpNioClient)mClient).init(mSocketChannel);
+                if(null != mNioConnectListener){
+                    mNioConnectListener.onConnectSuccess(SocketProcessor.this,mSocketChannel);
+                }
+
+                //开始读写
+                boolean isExit = false;
+                while(!isExit) {
+
+                    int readKeys = mSelector.select();
+                    if(readKeys > 0){
+                        Iterator<SelectionKey> selectedKeys = mSelector.selectedKeys().iterator();
+                        while (selectedKeys.hasNext()) {
+                            SelectionKey key =  selectedKeys.next();
+                            selectedKeys.remove();
+
+                            if (!key.isValid()) {
+                                continue;
+                            }
+
+                            if (key.isReadable()) {
+                                BaseClient mClient = (BaseClient) key.attachment();
+                                boolean ret = mClient.onRead();
+                                if(!ret){
+                                    isExit = true;
+                                    key.cancel();
+                                    key.attach(null);
+                                    key.channel().close();
+                                    break;
+                                }
+
+                            }else if (key.isWritable()) {
+                                BaseClient mClient = (BaseClient) key.attachment();
+                                boolean ret = mClient.onWrite();
+                                if(!ret){
+                                    isExit = true;
+                                    key.cancel();
+                                    key.attach(null);
+                                    key.channel().close();
+                                    break;
+                                }
+                                key.interestOps(SelectionKey.OP_READ);
+                            }
+                        }
+                    }
+
+                    if(isExit || closed){
+                        break;
+                    }
+
+                    if(!mClient.mWriteMessageQueen.mQueen.isEmpty()) {
+                        SelectionKey key= mSocketChannel.keyFor(mSelector);
+                        key.interestOps(SelectionKey.OP_WRITE);
+                    }
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            onSocketExit(1);
+        }
+    }
+}
