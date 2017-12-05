a462d22244803205cb9c5fee8be677c64a0c8003
Long <longhui.yang@inveno.cn>
2017-12-05 19:10:09 +0800

    init project !

--- app/src/main/java/com/open/test/net/NetMainActivity.java
+++ app/src/main/java/com/open/test/net/NetMainActivity.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
-
+import android.view.Window;
 
 
 public class NetMainActivity extends Activity {
@@ -13,6 +13,7 @@ public class NetMainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
         setContentView(R.layout.net_main);
         initView();
     }
