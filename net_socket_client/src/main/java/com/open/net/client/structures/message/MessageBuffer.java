4d999cc591b22e283b183d132b2915f0abf528ae
Long <longhui.yang@inveno.cn>
2017-12-08 11:22:20 +0800

    review code !

--- net_socket_client/src/main/java/com/open/net/client/structures/message/MessageBuffer.java
+++ net_socket_client/src/main/java/com/open/net/client/structures/message/MessageBuffer.java
@@ -12,55 +12,64 @@ import java.util.LinkedList;
 
 public final class MessageBuffer {
 
+    //----------------------------------------------------------------------------------------
+    //单元大小
     private static final int KB = 1024;
     private static final int MB = 1024*KB;
 
-    //块大小
-    private static int capacity_small  = 8   * KB;
-    private static int capacity_middle = 256 * KB;
-    private static int capacity_large  = 1   * MB;
+    //定义单块消息体内存大小
+    private static int capacity_small   = 8   * KB;
+    private static int capacity_middle  = 128 * KB;
+    private static int capacity_large   = 1   * MB;
 
     //块数量
-    private static int block_size_small  = 5;
-    private static int block_size_middle = 2;
-    private static int block_size_large  = 0;
+    private static int size_small       = 5;
+    private static int size_middle      = 2;
+    private static int size_large       = 0;
 
-    //buffer依赖的数组
+    //buffer依赖的实际数组
     private byte[] buffer_small  ;
     private byte[] buffer_middle ;
     private byte[] buffer_large  ;
 
-    //buffer可用性
-    private MessageBufferTracker buffer_small_tracker;
-    private MessageBufferTracker buffer_middle_tracker;
-    private MessageBufferTracker buffer_large_tracker;
+    //buffer可用性追踪
+    private MessageBufferTracker tracker_buffer_small;
+    private MessageBufferTracker tracker_buffer_middle;
+    private MessageBufferTracker tracker_buffer_large;
 
+    //----------------------------------------------------------------------------------------
     //临时可缓存对象
-    private final int MAX_QUEEN_SIZE = 8;
-    private LinkedList<byte[]> mLimitQueen = new LinkedList<>();
+    private final int MAX_TEMPORARY_CACHE_SIZE = 5;
+    private LinkedList<byte[]> mTemporaryCacheList = new LinkedList<>();
 
+    //----------------------------------------------------------------------------------------
+    //复用类型
+    private final int REUSE_SMALL   = 1;
+    private final int REUSE_MIDDLE  = 2;
+    private final int REUSE_LARGE   = 3;
+    private final int REUSE_TEMP    = 4;
 
-    public static void init (int buffer_small_capacity ,int buffer_middle_capacity, int buffer_large_capacity,
-                             int buffer_small_block_size, int buffer_middle_block_size, int buffer_large_block_size) {
+    //----------------------------------------------------------------------------------------
+    public static void init (int capacity_small ,int capacity_middle, int capacity_large,
+                             int size_small, int size_middle, int size_large) {
 
-        capacity_small     = buffer_small_capacity;
-        capacity_middle    = buffer_middle_capacity;
-        capacity_large =    buffer_large_capacity;
+        MessageBuffer.capacity_small    = capacity_small;
+        MessageBuffer.capacity_middle   = capacity_middle;
+        MessageBuffer.capacity_large    = capacity_large;
 
-        block_size_small   = buffer_small_block_size;
-        block_size_middle  = buffer_middle_block_size;
-        block_size_large   = buffer_large_block_size;
-
+        MessageBuffer.size_small        = size_small;
+        MessageBuffer.size_middle       = size_middle;
+        MessageBuffer.size_large        = size_large;
     }
 
     public MessageBuffer() {
-        buffer_small    = new byte[block_size_small  * capacity_small];
-        buffer_middle   = new byte[block_size_middle * capacity_middle];
-        buffer_large    = new byte[block_size_large  * capacity_large];
+        buffer_small    = new byte[size_small   * capacity_small];
+        buffer_middle   = new byte[size_middle  * capacity_middle];
+        buffer_large    = new byte[size_large   * capacity_large];
 
-        buffer_small_tracker    = new MessageBufferTracker(block_size_small);
-        buffer_middle_tracker   = new MessageBufferTracker(block_size_middle);
-        buffer_large_tracker    = new MessageBufferTracker(block_size_large);
+        tracker_buffer_small    = new MessageBufferTracker(size_small);
+        tracker_buffer_middle   = new MessageBufferTracker(size_middle);
+        tracker_buffer_large    = new MessageBufferTracker(size_large);
     }
 
     public Message build(byte[] src,int offset, int length){
@@ -74,133 +83,133 @@ public final class MessageBuffer {
         Message ret = MessagePool.get();
 
         if(length <= capacity_small){
-            ret.src_reuse_type = 1;
+            ret.src_reuse_type = REUSE_SMALL;
         }else if(length <= capacity_middle){
-            ret.src_reuse_type = 2;
+            ret.src_reuse_type = REUSE_MIDDLE;
         }else if(length <= capacity_large){
-            ret.src_reuse_type = 3;
+            ret.src_reuse_type = REUSE_LARGE;
         }else{
-            ret.src_reuse_type = 4;
+            ret.src_reuse_type = REUSE_TEMP;
         }
 
         if(length <= capacity_small){
-            int index = buffer_small_tracker.get();
-            if(index != -1){
-                ret.data = buffer_small;
-                ret.capacity = capacity_small;
-                ret.block_index   = index;
-                ret.offset   = capacity_small * index;
-                ret.length   = 0;
-                ret.dst_reuse_type = 1;
+            int block_index = tracker_buffer_small.get();
+            if(block_index != -1){
+                ret.block_index     = block_index;
+                ret.data            = buffer_small;
+                ret.capacity        = capacity_small;
+                ret.offset          = capacity_small * ret.block_index;
+                ret.length          = 0;
+                ret.dst_reuse_type  = REUSE_SMALL;
                 return ret;
             }
         }
 
         if(length <= capacity_middle){
-            int index = buffer_middle_tracker.get();
-            if(index != -1){
-                ret.data = buffer_middle;
-                ret.capacity = capacity_middle;
-                ret.block_index   = index;
-                ret.offset   = capacity_middle * index;
-                ret.length   = 0;
-                ret.dst_reuse_type = 2;
+            int block_index = tracker_buffer_middle.get();
+            if(block_index != -1){
+                ret.block_index     = block_index;
+                ret.data            = buffer_middle;
+                ret.capacity        = capacity_middle;
+                ret.offset          = capacity_middle * ret.block_index;
+                ret.length          = 0;
+                ret.dst_reuse_type  = REUSE_MIDDLE;
                 return ret;
             }
         }
 
         if(length <= capacity_large){
-            int index = buffer_large_tracker.get();
-            if(index != -1){
-                ret.data = buffer_large;
-                ret.capacity = capacity_large;
-                ret.block_index   = index;
-                ret.offset   = capacity_large * index;
-                ret.length   = 0;
-                ret.dst_reuse_type = 3;
+            int block_index = tracker_buffer_large.get();
+            if(block_index != -1){
+                ret.block_index     = block_index;
+                ret.data            = buffer_large;
+                ret.capacity        = capacity_large;
+                ret.offset          = capacity_large * ret.block_index;
+                ret.length          = 0;
+                ret.dst_reuse_type  = REUSE_LARGE;
                 return ret;
             }
         }
 
-        int limitQueenSize = mLimitQueen.size();
-        for (int i = 0;i<limitQueenSize;i++) {
-            if(length <= mLimitQueen.get(i).length){
-                ret.data = mLimitQueen.remove(i);
-                ret.capacity = ret.data.length;
-                ret.block_index   = -1;
-                ret.offset   = 0;
-                ret.length   = 0;
-                ret.dst_reuse_type = 4;
+        int mTemporaryCacheListSize = mTemporaryCacheList.size();
+        for (int i = 0;i<mTemporaryCacheListSize;i++) {
+            if(length <= mTemporaryCacheList.get(i).length){
+                ret.block_index     = 0;
+                ret.data            = mTemporaryCacheList.remove(i);
+                ret.capacity        = ret.data.length;
+                ret.offset          = 0;
+                ret.length          = 0;
+                ret.dst_reuse_type  = REUSE_TEMP;
                 return ret;
             }
         }
 
         //要么数据体太大，要么数据体全部用完了,自由创建数据
-        ret.data = new byte[length];
-        ret.capacity = length;
-        ret.block_index   = -1;
-        ret.offset   = 0;
-        ret.length   = 0;
-        ret.dst_reuse_type = 4;
+        ret.block_index     = 0;
+        ret.data            = new byte[length];
+        ret.capacity        = ret.data.length;
+        ret.offset          = 0;
+        ret.length          = 0;
+        ret.dst_reuse_type  = REUSE_TEMP;
 
         return ret;
     }
 
     public void release(Message msg){
-        if(msg.dst_reuse_type == 1){
-            buffer_small_tracker.release(msg.block_index);
+        if(msg.dst_reuse_type == REUSE_SMALL){
+            tracker_buffer_small.release(msg.block_index);
             MessagePool.put(msg);
-        }else if(msg.dst_reuse_type == 2){
-            buffer_middle_tracker.release(msg.block_index);
+        }else if(msg.dst_reuse_type == REUSE_MIDDLE){
+            tracker_buffer_middle.release(msg.block_index);
             MessagePool.put(msg);
-        }else if(msg.dst_reuse_type == 3){
-            buffer_large_tracker.release(msg.block_index);
+        }else if(msg.dst_reuse_type == REUSE_LARGE){
+            tracker_buffer_large.release(msg.block_index);
             MessagePool.put(msg);
-        }else{
-            if(MAX_QUEEN_SIZE>0){
-                while (mLimitQueen.size() >= MAX_QUEEN_SIZE){
-                    mLimitQueen.poll();
+        }else if(msg.dst_reuse_type == REUSE_TEMP){
+            if(MAX_TEMPORARY_CACHE_SIZE >0){
+                while (mTemporaryCacheList.size() >= MAX_TEMPORARY_CACHE_SIZE){
+                    mTemporaryCacheList.poll();
                 }
-                mLimitQueen.add(msg.data);
+                mTemporaryCacheList.add(msg.data);
                 msg.reset();
             }else{
-                mLimitQueen.clear();
+                mTemporaryCacheList.clear();
                 msg.reset();
             }
+        }else{
+            //------------------do nothing------------------
         }
     }
 
 
     public class MessageBufferTracker{
 
-        private int block_size;
+        private int size;
 
-        private byte[] available;
-        private int userd_count;
-        private int next_avaliable_index;
+        private byte[] array_available_index;//所有可用块的索引
+        private int used_count;//已经使用的数量
+        private int next_available_index;//下一个可以使用的位置
 
-        public MessageBufferTracker(int block_size){
-
-            this.block_size = block_size;
-
-            available = new byte[block_size];
-            for (int i = 0;i<block_size;i++){
-                available[i] = 1;
+        public MessageBufferTracker(int size){
+            this.size = size;
+            array_available_index = new byte[size];
+            for (int i = 0;i<size;i++){
+                array_available_index[i] = 1;
             }
-            userd_count = 0;
-            next_avaliable_index = 0;
+            used_count = 0;
+            next_available_index = 0;
         }
 
-        private int findFreeBlockIndex(){
-            if(userd_count < block_size){
-                for (int i = next_avaliable_index; i>=0 && i < block_size; i++) {
-                    if(available[i] == 1){
+        private int findAvailableIndex(){
+            if(used_count < size){
+                for (int i = next_available_index; i>=0 && i < size; i++) {
+                    if(array_available_index[i] == 1){
                         return i;
                     }
                 }
 
-                for (int i=0;i>=0 && i< next_avaliable_index;i++){
-                    if(available[i] == 1){
+                for (int i = 0; i>=0 && i< next_available_index; i++){
+                    if(array_available_index[i] == 1){
                         return i;
                     }
                 }
@@ -209,23 +218,27 @@ public final class MessageBuffer {
         }
 
         public int get(){
-            int index = findFreeBlockIndex();
+            int index = findAvailableIndex();
             if(index != -1){
-                available[index] = 0;//设置为已用
-                userd_count ++;//已用条数+1
+                array_available_index[index] = 0;//设置为已用
+                used_count++;//已用条数+1
 
-                next_avaliable_index++;//寻找下一条可能可用的位置
-                if(next_avaliable_index >= block_size){
-                    next_avaliable_index = 0;
+                next_available_index++;//寻找下一条可能可用的位置
+                if(next_available_index >= size){
+                    next_available_index = 0;
                 }
             }
             return index;
         }
 
         public void release(int index){
-            if(available[index] == 0){
-                available[index] = 1;
-                userd_count --;
+            if(index < 0 || index >= size){
+                //------------------error action------------------
+                return;
+            }
+            if(array_available_index[index] == 0){
+                array_available_index[index] = 1;
+                used_count--;
             }
         }
     }
